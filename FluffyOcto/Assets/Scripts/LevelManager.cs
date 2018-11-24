a74129c1b14e94312678d266c1311da1da3f94fc
Ondřej Paška <on.paska@gmail.com>
2018-11-24 12:01:24 +0100

    replace realties

---
+++ FluffyOcto/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public GameObject RealityOnRoot;
+	
+	public GameObject RealityOffRoot;
+	public ToggleButton Toggle;
+	private void Start()
+	{
+		RealityOffRoot.SetActive(false);
+		RealityOnRoot.SetActive(true);
+		Toggle.OnToggled.AddListener(ToggleReality);
+	}
+	
+	private void ToggleReality()
+	{
+		bool on = Toggle.Toggled;
+		Debug.Log(""+on);
+		RealityOffRoot.SetActive(!on);
+		RealityOnRoot.SetActive(on);
+	}
+}
---
+++ FluffyOcto/Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ToggleButton : MonoBehaviour
+{
+	public Sprite SpriteOn;
+
+	public Sprite SpriteOff;
+
+	private bool _toggled;
+	public bool Toggled => _toggled;
+
+	public UnityEvent OnToggled;
+	
+	private void OnMouseDown()
+	{
+		_toggled = !_toggled;
+		GetComponent<SpriteRenderer>().sprite = _toggled ? SpriteOn : SpriteOff;
+		OnToggled?.Invoke();
+	}
+}

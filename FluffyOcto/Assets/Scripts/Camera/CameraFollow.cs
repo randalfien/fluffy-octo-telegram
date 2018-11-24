2e401fcdd3a29253e7e064262eedabc4feb48629
Ondřej Paška <on.paska@gmail.com>
2018-11-24 14:39:09 +0100

    switching with swipe effect

---
+++ FluffyOcto/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	public Transform ToFollow;
+	public Transform FollowCamera;
+
+	public Vector3 Offset;
+	private Vector3 _speed;
+
+	private float _startZ;
+
+	private void Awake()
+	{
+		_startZ = FollowCamera.transform.localPosition.z;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		var targetPos = ToFollow.position + Offset;
+		var current = FollowCamera.transform.position;
+		current = Vector3.SmoothDamp(current, targetPos, ref _speed, 1f );
+		current.z = _startZ;
+		FollowCamera.transform.position = current;
+	}
+}

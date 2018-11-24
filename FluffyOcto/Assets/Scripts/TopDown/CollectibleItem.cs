f9fffe6e15d32e77b2e5d776f2312285ec97bdd9
Ondřej Paška <on.paska@gmail.com>
2018-11-24 18:11:13 +0100

    topdown first draft

---
+++ FluffyOcto/Assets/Scripts/TopDown/CollectibleItem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+	public GameObject TextObject;
+	public GameObject NextItem;
+	private void Start()
+	{
+		TextObject.SetActive(false);
+	}
+}

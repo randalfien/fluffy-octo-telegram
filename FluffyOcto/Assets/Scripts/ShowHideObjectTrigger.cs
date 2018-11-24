8672de236578658035455c2c7eb5cdc59ce8a25d
Petr Houška <houskape@gmail.com>
2018-11-24 22:02:51 +0100

    Finish old level.

--- FluffyOcto/Assets/Scripts/ShowHideObjectTrigger.cs
+++ FluffyOcto/Assets/Scripts/ShowHideObjectTrigger.cs
@@ -7,15 +7,33 @@ public class ShowHideObjectTrigger : MonoBehaviour
 
     public GameObject ObjectToShow;
     public bool HideOnTrigger = true;
+    public bool ChangeActiveOnStart = true;
+    public float Delay = 0;
 
+    private bool hit = false;
     private void Start()
     {
-        ObjectToShow.SetActive(HideOnTrigger);
+        if (ChangeActiveOnStart)
+        {
+            ObjectToShow.SetActive(HideOnTrigger);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        ObjectToShow.GetComponent<SpriteRenderer>().enabled = !HideOnTrigger;
+        if (!hit)
+        {
+            Invoke(nameof(onTriggerAfterDelay), Delay);
+            hit = true;
+        }
+
+    }
+
+    private void onTriggerAfterDelay()
+    {
+        SpriteRenderer spriteRenderer = ObjectToShow.GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null) { spriteRenderer.enabled = !HideOnTrigger; }
+        else { ObjectToShow.SetActive(!HideOnTrigger); }
     }
 
 }

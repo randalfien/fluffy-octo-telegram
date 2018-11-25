522b4a07714ea349b9807f0d7729375f4af020c6
Ondřej Paška <on.paska@gmail.com>
2018-11-25 21:01:40 +0100

    asdfa

---
+++ FluffyOcto/Assets/FollowY.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowY : MonoBehaviour
+{
+	public Transform ToFollow;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var pos = transform.localPosition;
+		pos.y = ToFollow.localPosition.y;
+		transform.localPosition = pos;
+	}
+}

062a8c574df4700de3fa8e13772b7b3d06556c67
Ondřej Paška <on.paska@gmail.com>
2018-11-25 11:17:53 +0100

    sex draft

---
+++ FluffyOcto/Assets/Scripts/FirstSex/Bubble1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble1 : MonoBehaviour
+{
+
+	public GameObject NextBubble;
+	
+	// Use this for initialization
+	void Start () {
+		NextBubble.SetActive(false);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			FindObjectOfType<RealityScheduler>().ScheduleMe(() => NextBubble.SetActive(true),0.8f,gameObject.layer);
+			gameObject.SetActive(false);
+		}
+	}
+}

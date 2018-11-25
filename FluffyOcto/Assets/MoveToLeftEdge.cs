7405d000432621c20ffa288f5a790934e186e82a
Ondřej Paška <on.paska@gmail.com>
2018-11-25 20:29:42 +0100

    lvl

---
+++ FluffyOcto/Assets/MoveToLeftEdge.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToLeftEdge : MonoBehaviour
+{
+
+	public float Offset; 
+	// Use this for initialization
+	void Start () {
+		var ratio = Screen.width / (float) Screen.height;
+		transform.localPosition = new Vector3(-54 * ratio + Offset, transform.localPosition.y, transform.localPosition.z);
+	}
+
+	private void Update()
+	{
+		Start();
+	}
+}

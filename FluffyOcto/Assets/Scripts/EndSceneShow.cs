6a86c02e0ebd3dd52f7a59bd61c620e227187d60
Ondřej Trhoň <ondrej@trhon.cz>
2018-11-25 19:48:54 +0100

    EndScreen pod mi

---
+++ FluffyOcto/Assets/Scripts/EndSceneShow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EndSceneShow : MonoBehaviour
+{
+
+	public GameObject ToShow;
+	public float Delay = 2f;
+	public float KeepOnScreen = 0;
+	
+	void Start ()
+	{
+		ToShow.SetActive(false);
+		var scheduler = FindObjectOfType<RealityScheduler>();
+		
+		Invoke(nameof(StartMsg), Delay);
+		if (KeepOnScreen != 0)
+		{
+			Invoke(nameof(Rem), Delay+KeepOnScreen);
+		}
+	}
+	
+	void StartMsg () {
+		ToShow.SetActive(true);
+	}
+	
+	void Rem () {
+		Destroy(ToShow);
+	}
+}

25b730d09d72ca6f66bdf41a2d2d52260e121c0c
Petr Houška <houskape@gmail.com>
2018-11-25 16:20:58 +0100

    Finished oldlevel + stub for grave level.

---
+++ FluffyOcto/Assets/Scripts/CollisionAdvProgressbar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAdvProgressbar : MonoBehaviour {
+
+    public ProgressBar Progress;
+    public float AdvanceBy = 0.25f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Progress.AddProgress(AdvanceBy);
+    }
+}
